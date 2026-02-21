694816b525a84b2173c97b822595886083982a54
Andrea-A-free <andrea.andena@gmail.com>
2026-02-21 09:37:34 +0100

    Update InitGui.py

--- RealVisualLight/InitGui.py
+++ RealVisualLight/InitGui.py
@@ -1,55 +1,29 @@
-import FreeCAD as App
-import FreeCADGui as Gui
-import RVL_Settings as S
+import FreeCADGui
+import FreeCAD
 
-def applyRealVisualLight():
-    """Applica la visualizzazione ottimizzata usando i parametri personalizzabili"""
-    try:
-        view = Gui.ActiveDocument.ActiveView
-    except:
-        return
+# ============================================================
+#  RealVisualLight - Workbench Integration
+# ============================================================
 
-    # Impostazioni di visualizzazione
-    view.setCameraType(S.CAMERA)
-    view.setAxisCross(S.AXIS)
-    view.setGrid(S.GRID)
-    view.setBackgroundColor(S.BACKGROUND)
-    view.setHighlightColor(S.HIGHLIGHT)
-    view.setSelectionColor(S.SELECTION)
+class RealVisualLightWorkbench(FreeCADGui.Workbench):
 
-    print("RealVisualLight: visualizzazione applicata")
-
-# --- Observer per apertura file ---
-class RVL_DocObserver:
-    def slotCreatedDocument(self, doc):
-        applyRealVisualLight()
-
-    def slotRestoredDocument(self, doc):
-        applyRealVisualLight()
-
-docObserver = RVL_DocObserver()
-App.addDocumentObserver(docObserver)
-
-# --- Observer per cambio Workbench ---
-def onWorkbenchActivated():
-    applyRealVisualLight()
-
-Gui.getMainWindow().workbenchActivated.connect(onWorkbenchActivated)
-
-# --- Registrazione Workbench ---
-class RealVisualLightWorkbench(Gui.Workbench):
-    MenuText = "Real Visual Light"
-    ToolTip = "Motore di visualizzazione ottimizzato"
-    Icon = "resources/icons/rvl_icon.svg"
+    MenuText = "RealVisualLight"
+    ToolTip = "Ottimizzazione automatica della visualizzazione"
+    Icon = """
+    /* SVG ICON */
+    """
 
     def Initialize(self):
+        FreeCAD.Console.PrintMessage("[RealVisualLight] Workbench attivato.\n")
+
+    def Activated(self):
+        FreeCAD.Console.PrintMessage("[RealVisualLight] Analisi automatica attiva.\n")
+
+    def Deactivated(self):
         pass
 
     def GetClassName(self):
         return "Gui::PythonWorkbench"
 
-Gui.addWorkbench(RealVisualLightWorkbench())
 
-
-
-
+FreeCADGui.addWorkbench(RealVisualLightWorkbench())
